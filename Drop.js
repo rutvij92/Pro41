807e77b36daddea9912d30d3b63f0bc59df8b49a
rutvij92 <69469721+rutvij92@users.noreply.github.com>
2020-10-21 18:54:58 +0530

    Add files via upload

---
+++ Drop.js
@@ -0,0 +1,17 @@
+class Drop {
+    constructor(x,y,radius){
+ var   options={
+        friction:0.1
+    }
+        this.body = Bodies.circle(x,y,5,options);
+        World.add(world,this.body);
+    }
+    display(){
+
+ellipseMode(RADIUS);
+fill("blue");
+ellipse(this.body.position.x,this.body.position.y,5,5);
+
+    }
+    
+    }
